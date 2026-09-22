359737150ec989bc0d0a65e613137a1ac6c893a1
ADEYINKE-30 <adepojuadeyinka2020@gmail.com>
2026-09-22 23:08:50 +0100

    Update CORS to allow Netlify frontend

--- src/app.ts
+++ src/app.ts
@@ -24,9 +24,12 @@ dotenv.config();
 const app = express();
 
 // app.ts
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
+   app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://devnotesade.netlify.app'
+  ],
+  credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
